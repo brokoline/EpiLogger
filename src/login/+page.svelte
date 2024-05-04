b370151ceb528f79356d073fbf8aa3c0e4c419e3
brokoline <125354647+brokoline@users.noreply.github.com>
2024-05-04 10:58:14 +0200

    Første commit

---
+++ src/login/+page.svelte
@@ -0,0 +1,3 @@
+<Input bind:value={username} type="text" placeholder="Indtast E-mail" />
+<Input bind:value={password} type="password" placeholder="Indtast kodeord" />
+<Button color="primary" on:click={login}>Login</Button>
